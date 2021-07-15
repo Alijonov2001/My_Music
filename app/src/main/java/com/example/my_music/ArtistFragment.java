79640820eb0778491a7bc23993d5ee4b2f3eedd8
Oybek <oybekalijonov2001@gmail.com>
2021-07-15 23:11:33 +0500

    Toast qo'shdim

--- app/src/main/java/com/example/my_music/ArtistFragment.java
+++ app/src/main/java/com/example/my_music/ArtistFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 public class ArtistFragment extends Fragment {
@@ -17,6 +18,7 @@ public class ArtistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Toast.makeText(getContext(), "hello", Toast.LENGTH_SHORT).show();;
         return inflater.inflate(R.layout.fragment_artist, container, false);
     }
 }
